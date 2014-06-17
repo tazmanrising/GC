3ce4187bc4c8806859112354a7c508ab8ee0e3fc
Chris Badahdah <a.s.badahdah@gmail.com>
2014-06-16 21:43:39 -0700

    WIP: added new styles and pagination is working.

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'mgcrea.ngStrap.popover'])
+var app = angular.module('app', ['ngRoute'])
 .config(['$routeProvider',
   function ($routeProvider) {
     $routeProvider
@@ -11,6 +11,7 @@ var app = angular.module('app', ['ngRoute', 'mgcrea.ngStrap.popover'])
 app.service('Data', ['$http', '$q',
 function ($http, $q) {
 
+  /*
   var user = $q.defer(); 
 
   $http.get('/api/user').success(function (data) {
@@ -27,6 +28,7 @@ function ($http, $q) {
     user: user.promise,
     codes: codes.promise
   };
+  */
 
 }]);
 
@@ -35,106 +37,156 @@ app.controller('SearchCtrl', ['$scope', '$routeParams', 'Data',
 
 function ($scope, $routeParams, Data) {
 
-  $scope.match = {}; // setup the default match object
-  $scope.results = []; // results based on match object
+  $scope.data = [];
+  for (var i = 0; i < 5000; i++) {
+    $scope.data.push({
+      index: i,
+      value: i + 1,
+      random: Math.ceil(Math.random() * 100)
+    });
+  }
 
-  $scope.message = "hello";
+  $scope.match = []; // setup the default match object
+  $scope.results = $scope.data;
 
+/*
   Data.codes.then(function (codes) {
     $scope.data = codes;
     $scope.results = codes;
-  });
+  });*/
 
-  $scope.hello = {
-    keys: [
-      { field: 'id', css: { 'width': '40px' } },
-      { field: 'location' },
-      { field: 'address' },
-      { field: 'district' },
-      { field: 'forty' },
-      { field: 'code' }
-    ]
-  };
+  $scope.keys = [
+    { field: 'id', css: { 'width': '40px' } },
+    { field: 'location' },
+    { field: 'address' },
+    { field: 'district' },
+    { field: 'forty' },
+    { field: 'code' }
+  ];
+
+  $scope.searchFields = [
+    'index',
+    'location',
+    'address',
+    'district',
+    'forty',
+    'code'
+  ];
 
   // watch gets triggered anytime the match object changes; 
   // the user changes their search paramaeters.
   $scope.$watch('match', function () {
-    // makes it easier to deal with when empty string option is set.
-    for (var key in $scope.match) {
-      if ($scope.match[key] == "") delete $scope.match[key];
-    }
+
     // try to filter out the result based on match object
-    var results = _.filter($scope.data, function (item) {
+    var results = _.filter($scope.data, function (element) {
       var res = false; // assume current option doesn't match
-      for (var key in $scope.match) {
-        // to lower case to ignore case
-        _.each($scope.match[key].toLowerCase().split(' '), function (keyword) {
-          if (String(item[key]).toLowerCase().indexOf(keyword) != -1) {
-            res = true;
-          }
-        });
+      for (var i = 0; i < $scope.match.length; i++) {
+        var obj = $scope.match[i];
+        if (obj)
+        if (String(element[obj.key]).indexOf(obj.value) > -1 || !obj.value) {
+          res = true;
+        }
       }
       return res; // return result
     });
+    //console.log(results.length);
     // if no match is found, display all results
-    $scope.results = (results.length > 0) ? results : $scope.data;
+    //$scope.results = (results.length > 0) ? results : $scope.data;
+    $scope.results = results;
   }, true);
 
-  // quickly clear the search form
-  $scope.clear = function () { $scope.match = {}; };
-  // check if search is empty or has been reset
-  $scope.isEmpty = function () {
-    return Object.keys($scope.match).length == 0;
-  };
+}]);
 
+// directive for building search queries with predefined parameters
+// input:
+//  params - array that represents search parameters; the will show up
+//  in the dropdown to associate with each search term. the default is
+//  blank.
+//  ex: ['id', 'param1', ...]
+// output:
+//  search - array that represenets the search from the user.
+//  ex: [{key: id, value: "0"}, {key: param1, value: hello}, ...]
+app.directive('searchBuilder', function () {
+  return {
+    scope: {
+      params: '=',  
+      search: '=' 
+    },  
+    templateUrl: 'partials/searchBuilder.html',
+    restrict: 'EA',
+    link: function (scope, el, attrs) {
 
-} ]);
+      // by default there is a single search item which is empty.
+      scope.search = [{}];
+
+      // add parameter to the search
+      scope.add = function () {
+        scope.search.push({});
+      }   
+
+      // remove a parameter from the search
+      scope.remove = function (i) {
+        scope.search.splice(i,1);
+      }   
+    }   
+  };  
+})  
+
 
 // Awesome directive to paginate an array.
 app.directive('ngPaginate', function () {
   return {
     transclude: true,
     restrict: 'AE',
-    scope: {
-      variable: '='
-    },
+    scope: true,
     link: function ($scope, $el, $attrs, ctrl, $transclude) {
+
       // html code that is nested in the directive will have the scope
       // of the directive. consult: 
       // http://stackoverflow.com/questions/20878830/angularjs-isolated-scope-for-directives-without-own-template
       // for description as to why this works exactly
-      $transclude($scope, function(clone) {
-        $el.append(clone);
-      });
-    },
-    controller: ['$scope', function ($scope) {
+      $transclude($scope, function(clone) { $el.append(clone); });
 
-      var calculateVariables = function () {
-        if ($scope.variable && $scope.variable.length > 0) {
-          var items = $scope.items = $scope.variable.length;
+      // array of attributes delimited by white space passed to 
+      // ngPaginate.
+      // format: page in pages
+      var attrs = $attrs.ngPaginate.match(/(.*) in (.*)/);
+
+      // setup scope variables
+      var pageName = $scope.pageName = attrs[1];
+      var arrayName = $scope.arrayName = attrs[2];
+      var itemsPerPage = $scope.itemsPerPage = 10;
+
+      var slice = function () {
+        if (!!$scope.$parent[$scope.arrayName]) {
+          var currentIndex = $scope.currentIndex = ($scope.currentPage - 1)*$scope.itemsPerPage;
+          var nextIndex = currentIndex + Number($scope.itemsPerPage);
+          $scope[pageName] = $scope.$parent[$scope.arrayName].slice(currentIndex, nextIndex);
+        }
+      };
+
+      var setup = function () {
+
+        var currentPage = $scope.currentPage = 1;
+
+        if ($scope[arrayName].length && $scope[arrayName].length > 0) {
+          var items = $scope.items = $scope[arrayName].length;
         } else {
           var items = $scope.items = 0;
         }
 
-        // pagination variables (should be accessible in nested html)
-        var currentPage = $scope.currentPage = 1;
-        var itemsPerPage = $scope.itemsPerPage = 50;
+        $scope.maxPages = Math.ceil($scope.items/$scope.itemsPerPage);
 
-        $scope.maxPages = Math.ceil(items/itemsPerPage);
+        slice();
       };
 
-      $scope.$watch('variable', function () {
-        calculateVariables();
-        if (!!$scope.variable) {
-          $scope.data = $scope.variable.slice( ($scope.currentPage - 1) * $scope.itemsPerPage, $scope.itemsPerPage);
-        }
-      } , true);
+      // update view when array changes
+      $scope.$parent.$watch(arrayName, function () { setup(); } , true);
 
-      $scope.$watch('currentPage', function () {
-        if (!!$scope.variable) {
-          $scope.data = $scope.variable.slice( ($scope.currentPage - 1) * $scope.itemsPerPage, $scope.itemsPerPage);
-        }
-      });
+      // update view when pagination variables change
+      $scope.$watch('currentPage', function () { slice(); });
+      $scope.$watch('itemsPerPage', function () { setup(); });
+
 
       // move through the pages, if you reach either end, you will loop
       // around to begging/end
@@ -157,6 +209,8 @@ app.directive('ngPaginate', function () {
       // go to last page
       $scope.last = function () { $scope.currentPage = $scope.maxPages; };
 
-    }]
+      $scope.goto = function (page) { $scope.currentPage = page; };
+
+    }
   };
 });
